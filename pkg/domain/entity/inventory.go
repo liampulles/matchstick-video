114e535b02863f2fdaf59abb9c678c7c58cbe358
Liam Pulles <me@liampulles.com>
2020-07-15 20:49:39 +0200

    Make InventoryItem.available private

--- pkg/domain/entity/inventory.go
+++ pkg/domain/entity/inventory.go
@@ -4,26 +4,27 @@ import "fmt"
 
 // InventoryItem defines a unique InventoryItem
 type InventoryItem struct {
-	ID        ID
-	Name      string
-	Location  string
-	Available bool
+	ID       ID
+	Name     string
+	Location string
+
+	available bool
 }
 
 // IsAvailable will return true if the inventory item may
 // be checked out - false otherwise.
 func (i *InventoryItem) IsAvailable() bool {
-	return i.Available
+	return i.available
 }
 
 // Checkout will mark the inventory item as unavilable.
 // If the inventory item is not available,
 // then an error is returned.
 func (i *InventoryItem) Checkout() error {
-	if !i.Available {
+	if !i.available {
 		return fmt.Errorf("cannot check out inventory item - it is unavailable")
 	}
-	i.Available = false
+	i.available = false
 	return nil
 }
 
@@ -31,9 +32,9 @@ func (i *InventoryItem) Checkout() error {
 // If the inventory item is available, then an
 // error is returned.
 func (i *InventoryItem) CheckIn() error {
-	if i.Available {
+	if i.available {
 		return fmt.Errorf("cannot check in inventory item - it is already checked in")
 	}
-	i.Available = true
+	i.available = true
 	return nil
 }
