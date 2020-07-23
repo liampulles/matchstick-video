049cb761d531888fdc440feaa911bd53d51ef324
Liam Pulles <me@liampulles.com>
2020-07-24 00:03:56 +0200

    Impl ResponseFactory

--- pkg/adapter/http/response.factory.go
+++ pkg/adapter/http/response.factory.go
@@ -1,6 +1,9 @@
 package http
 
 import (
+	"strconv"
+
+	"github.com/liampulles/matchstick-video/pkg/domain/commonerror"
 	"github.com/liampulles/matchstick-video/pkg/domain/entity"
 )
 
@@ -10,4 +13,44 @@ type ResponseFactory interface {
 	Create(statusCode uint, body []byte) *Response
 	CreateFromError(error) *Response
 	CreateFromEntityID(statusCode uint, id entity.ID) *Response
+}
+
+// ResponseFactoryImpl implements ResponseFactory
+type ResponseFactoryImpl struct{}
+
+// Check that we implement the interface
+var _ ResponseFactory = &ResponseFactoryImpl{}
+
+// NewResponseFactoryImpl is a constructor
+func NewResponseFactoryImpl() *ResponseFactoryImpl {
+	return &ResponseFactoryImpl{}
+}
+
+// Create implements ResponseFactory
+func (r *ResponseFactoryImpl) Create(statusCode uint, body []byte) *Response {
+	return &Response{
+		StatusCode: statusCode,
+		Body:       body,
+	}
+}
+
+// CreateFromError implements ResponseFactory
+func (r *ResponseFactoryImpl) CreateFromError(err error) *Response {
+	switch v := err.(type) {
+	// TODO: Add your specific error handlers for controllers here.
+	case *commonerror.Validation:
+		return r.create(400, v.Error())
+	default:
+		return r.create(501, v.Error())
+	}
+}
+
+// CreateFromEntityID implements ResponseFactory
+func (r *ResponseFactoryImpl) CreateFromEntityID(statusCode uint, id entity.ID) *Response {
+	str := strconv.FormatInt(int64(id), 10)
+	return r.create(statusCode, str)
+}
+
+func (r *ResponseFactoryImpl) create(statusCode uint, body string) *Response {
+	return r.Create(statusCode, []byte(body))
 }
---
+++ test/pkg/adapter/http/response.factory_test.go
@@ -0,0 +1,90 @@
+package http_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+
+	"github.com/liampulles/matchstick-video/pkg/adapter/http"
+	"github.com/liampulles/matchstick-video/pkg/domain/commonerror"
+	"github.com/liampulles/matchstick-video/pkg/domain/entity"
+)
+
+type ResponseFactoryImplTestSuite struct {
+	suite.Suite
+	sut *http.ResponseFactoryImpl
+}
+
+func TestResponseFactoryImplTestSuite(t *testing.T) {
+	suite.Run(t, new(ResponseFactoryImplTestSuite))
+}
+
+func (suite *ResponseFactoryImplTestSuite) SetupTest() {
+	suite.sut = http.NewResponseFactoryImpl()
+}
+
+func (suite *ResponseFactoryImplTestSuite) TestCreate_ShouldCreateResponse() {
+	// Setup expectations
+	expected := &http.Response{
+		StatusCode: 501,
+		Body:       []byte("some.data"),
+	}
+
+	// Exercise SUT
+	actual := suite.sut.Create(501, []byte("some.data"))
+
+	// Verify results
+	suite.Equal(expected, actual)
+}
+
+func (suite *ResponseFactoryImplTestSuite) TestCreateFromError_WhenIsValidationError_ShouldReturnBadRequest() {
+	// Setup fixture
+	fixture := &commonerror.Validation{
+		Field:   "id",
+		Problem: "not numeric",
+	}
+
+	// Setup expectations
+	expected := &http.Response{
+		StatusCode: 400,
+		Body:       []byte("validation error: field=[id], problem=[not numeric]"),
+	}
+
+	// Exercise SUT
+	actual := suite.sut.CreateFromError(fixture)
+
+	// Verify results
+	suite.Equal(expected, actual)
+}
+
+func (suite *ResponseFactoryImplTestSuite) TestCreateFromError_WhenIsArbitraryError_ShouldReturnInternalServerError() {
+	// Setup fixture
+	fixture := fmt.Errorf("some.error")
+
+	// Setup expectations
+	expected := &http.Response{
+		StatusCode: 501,
+		Body:       []byte("some.error"),
+	}
+
+	// Exercise SUT
+	actual := suite.sut.CreateFromError(fixture)
+
+	// Verify results
+	suite.Equal(expected, actual)
+}
+
+func (suite *ResponseFactoryImplTestSuite) TestCreateFromEntityID_ShouldReturnIdAsString() {
+	// Setup expectations
+	expected := &http.Response{
+		StatusCode: 201,
+		Body:       []byte("101"),
+	}
+
+	// Exercise SUT
+	actual := suite.sut.CreateFromEntityID(201, entity.ID(101))
+
+	// Verify results
+	suite.Equal(expected, actual)
+}
