b4a30edb649992c888c57260f1c3c52fe9e31ea8
Liam Pulles <me@liampulles.com>
2020-07-23 22:27:19 +0200

    Impl dto.Factory

--- pkg/adapter/http/json/dto/factory.go
+++ pkg/adapter/http/json/dto/factory.go
@@ -6,3 +6,23 @@ import "github.com/liampulles/matchstick-video/pkg/domain/entity"
 type Factory interface {
 	CreateInventoryItemViewFromEntity(entity.InventoryItem) *InventoryItemView
 }
+
+// FactoryImpl implements Factory
+type FactoryImpl struct{}
+
+var _ Factory = &FactoryImpl{}
+
+// NewFactoryImpl is a constructor
+func NewFactoryImpl() *FactoryImpl {
+	return &FactoryImpl{}
+}
+
+// CreateInventoryItemViewFromEntity implements Factory
+func (f *FactoryImpl) CreateInventoryItemViewFromEntity(e entity.InventoryItem) *InventoryItemView {
+	return &InventoryItemView{
+		ID:        e.ID(),
+		Name:      e.Name(),
+		Location:  e.Location(),
+		Available: e.IsAvailable(),
+	}
+}
